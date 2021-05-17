d8f0bad296a2c70a61c11f0b452be656dbde1afc
muriloalvesdev <muriloalvesdev.ti@gmail.com>
2021-05-17 06:15:42 -0300

    init project

---
+++ src/main/java/br/com/simios/SimiosApplication.java
@@ -0,0 +1,13 @@
+package br.com.simios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimiosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/simios/SimiosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.simios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
