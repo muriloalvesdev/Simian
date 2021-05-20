a90d21353070469bd4f67c3b0e953ca2436ed99f
muriloalvesdev <muriloalvesdev.ti@gmail.com>
2021-05-20 14:30:37 -0300

    controllers

---
+++ src/main/java/br/com/simian/controller/SimianController.java
@@ -0,0 +1,35 @@
+package br.com.simian.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.simian.controller.handler.Handler;
+import br.com.simian.domain.model.Statistic;
+import br.com.simian.dto.DNA;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@RequestMapping("simian")
+@RestController
+class SimianController {
+
+	private final Handler handler;
+	static final String NOT_SIMIAN = "";
+
+	@PostMapping
+	public ResponseEntity<Object> isSimian(@RequestBody DNA dna) {
+		this.handler.validate(dna.getDna());
+		return ResponseEntity.ok().build();
+	}
+
+	@GetMapping
+	public ResponseEntity<Statistic> statistic() {
+		return ResponseEntity.ok(this.handler.statistic());
+	}
+
+}
---
+++ src/main/java/br/com/simian/controller/handler/Handler.java
@@ -0,0 +1,47 @@
+package br.com.simian.controller.handler;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+import br.com.simian.domain.model.Statistic;
+import br.com.simian.exception.DNAInvalidException;
+import br.com.simian.exception.DNANotSimianException;
+import br.com.simian.service.SimianService;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@Component
+public class Handler {
+	private static final String DNA_INVALID_MSG = "DNA must contain the following letters: A, T, C, G - Check that all letters are being entered.";
+
+	private final SimianService service;
+
+	public void validate(String[] dna) {
+		Arrays.asList(dna).forEach(value -> {
+			if (!isValid(value)) {
+				throw new DNAInvalidException(DNA_INVALID_MSG);
+			}
+		});
+		if (!this.service.isSimian(dna)) {
+			throw new DNANotSimianException("Not recognized as simian.");
+		}
+	}
+
+	public Statistic statistic() {
+		return this.service.statistic();
+	}
+
+	private static boolean isValid(String dna) {
+		System.out.println("value: ".concat(dna).concat(" - regex repeating: ").concat(
+				String.valueOf(dna.chars().boxed().collect(Collectors.groupingBy(value -> value, Collectors.counting()))
+						.values().stream().anyMatch(value -> value > 3))));
+		if (!dna.contains("A") && !dna.contains("T") && !dna.contains("C") && !dna.contains("G")) {
+			return false;
+		}
+		return (dna.contains("A") || dna.contains("T") || dna.contains("C") || dna.contains("G"));
+	}
+
+}
